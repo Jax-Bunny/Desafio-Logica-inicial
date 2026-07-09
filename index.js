1839ffc0075da390efbb725ebd725d26734cd6f4
Jax-Bunny <linsdouradog@gmail.com>
2026-07-09 17:41:05 -0300

    Meu primeiro commit!

---
+++ index.js
@@ -0,0 +1,39 @@
+let agente = "Cypher"
+
+let XP = 57
+
+if(XP <= 1000){
+    XP = "Ferro"
+    console.log("O agente " + agente + " está no ranking " + XP + "!")
+}else if(XP > 1000 && XP <= 2000){
+    XP = "Bronze"
+    console.log("O agente " + agente + " está no ranking " + XP + "!")
+}else if(XP > 2000 && XP <= 5000){
+    XP = "Prata"
+    console.log("O agente " + agente + " está no ranking " + XP + "!")
+}else if(XP > 5000 && XP <= 7000){
+    XP = "Ouro"
+    console.log("O agente " + agente + " está no ranking " + XP + "!")
+}else if(XP > 7000 && XP <= 8000){
+    XP = "Platina"
+    console.log("O agente " + agente + " está no ranking " + XP + "!")
+}else if(XP > 8000 && XP <= 9000){
+    XP = "Ascendente"
+    console.log("O agente " + agente + " está no ranking " + XP + "!")
+}else if(XP > 9000 && XP <= 10000){
+    XP = "Imortal"
+    console.log("O agente " + agente + " está no ranking " + XP + "!")
+}else if(XP >= 10001){
+    XP = "Radiante"
+    console.log("O agente " + agente + " está no ranking " + XP + "!")
+}
+
+
+
+
+
+
+
+
+
+
