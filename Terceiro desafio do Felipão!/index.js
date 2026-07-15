36d236ecbbf7184c5cd5815cb28b5ef1141f31e4
Jax-Bunny <linsdouradog@gmail.com>
2026-07-15 16:48:45 -0300

    Atualização

---
+++ Terceiro desafio do Felipão!/index.js
@@ -0,0 +1,56 @@
+let heroi = {
+  nome: "Aldrick Gallahad De Blackthorne",
+  idade: 57,
+  tipo: "Cavaleiro",
+  ataque: "Espadada"
+};
+
+if (heroi.tipo === "Cavaleiro" || heroi.tipo === "cavaleiro") {
+  console.log("O " + heroi.tipo + " atacou com uma " + heroi.ataque + "!");
+}else if (heroi.tipo === "Mago" || heroi.tipo === "mago") {
+    ataque = "Magia";
+  console.log("O " + heroi.tipo + " atacou com uma " + ataque + "!");
+}else if (heroi.tipo === "Ninja" || heroi.tipo === "ninja") {
+    ataque = "Shuriken";
+  console.log("O " + heroi.tipo + " atacou com uma " + ataque + "!");
+}else if (heroi.tipo === "Monge" || heroi.tipo === "monge") {
+    ataque = "Artes Marciais";
+  console.log("O " + heroi.tipo + " atacou usando suas " + ataque + "!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
